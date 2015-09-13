c3b9cb54d549d213499171668dd4cb74e027ba21
acolas1 <acolas1@ufl.edu>
2015-09-13 18:35:57 -0400

    updated delete checked function and also remaining function

--- todo-src/script.js
+++ todo-src/script.js
@@ -57,16 +57,29 @@ myApp.controller('MainCtrl', function($scope) {
         $scope.currentEditIndex = -1;
         $scope.submitText = "";
     }
-
+   /*Got code idea from http://jsfiddle.net/icoxfog417/nLC3g/ this will delete all the ones checked off*/
     $scope.deleteAll = function() {
         console.log("Deleting all");
-        $scope.todos.length = 0;
-    }
+         var original = $scope.todos;
+        $scope.todos = [];
+        angular.forEach(original, function(todo) {
+      if (!todo.done) $scope.todos.push(todo);
+    })
+  }
+    
 
     $scope.checkIndex = function(item) {
         return $scope.currentEditIndex == $scope.todos.indexOf(item);
     }
 
+    /*http://jsfiddle.net/icoxfog417/nLC3g this will count the remaining non checked items*/
+    $scope.remaining = function() {
+    var count = 0;
+    angular.forEach($scope.todos, function(todo) {
+      count += todo.done ? 0 : 1;
+    })
+    return count;
+  }
 
 });
 
